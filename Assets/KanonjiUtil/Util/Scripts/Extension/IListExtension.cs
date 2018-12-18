466bfa3f079957622c65f3e7cefced53467e5a5c
kanonji <kanonji@users.noreply.github.com>
2018-12-18 23:52:05 +0900

    Add IListExtension

---
+++ Assets/KanonjiUtil/Util/Scripts/Extension/IListExtension.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kanonji.Util.Extension {
+	public static class IListExtension {
+
+		public static T GetRandom<T>(this IList<T> list) {
+			return list[Random.Range(0, list.Count)];
+		}
+	}
+}
