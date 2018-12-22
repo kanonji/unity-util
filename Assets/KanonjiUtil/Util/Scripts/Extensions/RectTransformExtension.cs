5e2b893d045aa18c1b1e92646d8332a19d95f53f
kanonji <kanonji@users.noreply.github.com>
2018-12-23 02:18:32 +0900

    Add RectTransformExtension

---
+++ Assets/KanonjiUtil/Util/Scripts/Extensions/RectTransformExtension.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Kanonji.Util.Extensions {
+
+	public static class RectTransformExtension {
+
+		public static bool IsStretchedX(this RectTransform rectTransform) {
+			return 0 == rectTransform.anchorMin.x && 1 == rectTransform.anchorMax.x;
+		}
+
+		public static bool IsStretchedY(this RectTransform rectTransform) {
+			return 0 == rectTransform.anchorMin.y && 1 == rectTransform.anchorMax.y;
+		}
+
+		public static bool IsStretchedBoth(this RectTransform rectTransform) {
+			return rectTransform.IsStretchedX() && rectTransform.IsStretchedY();
+		}
+	}
+}
